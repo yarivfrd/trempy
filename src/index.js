4ad02b0262a028c450751d2a5e315e54d75fd871
Yariv Fruend <yarivfrd@gmail.com>
2022-07-25 23:35:12 +0300

    configured gh-pages and fixed prod

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
-    <Router>
+    <Router basename="/trempy">
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
